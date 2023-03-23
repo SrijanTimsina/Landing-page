1a13182cf3e2fce565323683c37b66d22b9b9bac
SrijanTimsina <srijan.timsina.39@gmail.com>
2023-03-23 22:14:21 +0545

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 const viewportHeight = window.innerHeight;
+
+var currentIndex = 0;
+let prevScrollPos = window.pageYOffset;
+let last = 0;
+const innerContainer = document.querySelectorAll(".inner-container");
+
+function scrollToSmoothly(pos, time) {
+	var currentPos = window.pageYOffset;
+	var start = null;
+	if (time == null) time = 500;
+	(pos = +pos), (time = +time);
+	window.requestAnimationFrame(function step(currentTime) {
+		start = !start ? currentTime : start;
+		var progress = currentTime - start;
+		if (currentPos < pos) {
+			window.scrollTo(
+				0,
+				((pos - currentPos) * progress) / time + currentPos
+			);
+		} else {
+			window.scrollTo(
+				0,
+				currentPos - ((currentPos - pos) * progress) / time
+			);
+		}
+		if (progress < time) {
+			window.requestAnimationFrame(step);
+		} else {
+			window.scrollTo(0, pos);
+		}
+	});
+}
+
+window.onscroll = function (e) {
+	var up = prevScrollPos > this.scrollY;
+	prevScrollPos = this.scrollY;
+	const now = new Date().getTime();
+	if (now - last > 1000) {
+		last = now;
+		if (currentIndex > 0 && up) {
+			innerContainer[currentIndex].classList.add("hide");
+			setTimeout(() => {
+				innerContainer[currentIndex + 1].classList.remove("active");
+				innerContainer[currentIndex + 1].classList.remove("hide");
+			}, 500);
+			currentIndex--;
+			innerContainer[currentIndex].classList.add("active");
+			scrollToSmoothly(viewportHeight * currentIndex, 500);
+		}
+		if (currentIndex < 4 && !up) {
+			innerContainer[currentIndex].classList.add("hide");
+			setTimeout(() => {
+				innerContainer[currentIndex - 1].classList.remove("active");
+				innerContainer[currentIndex - 1].classList.remove("hide");
+			}, 500);
+			currentIndex++;
+			innerContainer[currentIndex].classList.add("active");
+			scrollToSmoothly(viewportHeight * currentIndex, 500);
+		}
+	}
+};
